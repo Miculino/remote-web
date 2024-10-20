8b6c459bc9ce7b097d83d5517e6457f0de56ab2e
RemusDB <remusd.buhaianu@gmail.com>
2024-10-20 10:26:28 +0300

    feat(social proof): add star ratings

---
+++ app/(site)/components/Ratings.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Ratings() {
+  return (
+    <div className="flex items-center gap-8 justify-center">
+      <Image
+        src={"/TrustPilot_Stars.svg"}
+        alt="TrustPilot Stars Rating"
+        width={224}
+        height={28}
+      />
+      <Image
+        src={"/Capterra_Stars.svg"}
+        alt="Capterra Stars Rating"
+        width={217}
+        height={24}
+      />
+      <Image
+        src={"/G2_Stars.svg"}
+        alt="G2 Stars Rating"
+        width={145}
+        height={30}
+      />
+    </div>
+  );
+}
--- app/(site)/layout/SocialProof.tsx
+++ app/(site)/layout/SocialProof.tsx
@@ -1,10 +1,12 @@
 // Components
+import Ratings from "../components/Ratings";
 import SocialProofCard from "../components/SocialProofCard";
 
 export default function SocialProof() {
   return (
-    <section className="bg-dark-gray text-black py-10 pb-20">
+    <section className="bg-dark-gray text-black py-10 pb-20 flex flex-col gap-10">
       <SocialProofCard />
+      <Ratings />
     </section>
   );
 }
