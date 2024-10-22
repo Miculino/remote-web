45dcaaed1f480e90ac17591a851dff74df6c6c00
RemusDB <remusd.buhaianu@gmail.com>
2024-10-22 11:06:17 +0300

    chore: add hr library list constant file

---
+++ app/constants/hrLibraryList.ts
@@ -0,0 +1,31 @@
+export const HR_LIBRARY_LIST = [
+  {
+    title: "Research",
+    description:
+      "See the latest data on employment trends, jobseeker needs, and distributed work.",
+    illustration: "/product_showcase/Research_View.svg",
+  },
+  {
+    title: "Blog",
+    description: "Read insights from Remote's leaders and global HR experts.",
+    illustration: null,
+  },
+  {
+    title: "Employee Cost Calculator",
+    description:
+      "Salary is just the start. Get the real numbers on total employment costs around the world.",
+    illustration: "/product_showcase/ECC_View.svg",
+  },
+  {
+    title: "Country Explorer",
+    description:
+      "Country-specific guides on everything you need to know to employ abroad.",
+    illustration: "/product_showcase/CE_View.svg",
+  },
+  {
+    title: "Misclassification Risk Tool",
+    description:
+      "Contractor or employee? Understand your risk exposure to make the right choice every time.",
+    illustration: "/product_showcase/Misclass_View.svg",
+  },
+];
