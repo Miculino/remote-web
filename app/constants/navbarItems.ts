d4f8d364b12bed9417e13dc35e1492d4fa30f36d
RemusDB <remusd.buhaianu@gmail.com>
2024-10-22 22:06:15 +0300

    chore: add navbar items links

---
+++ app/constants/navbarItems.ts
@@ -0,0 +1,270 @@
+export const NAVBAR_ITEMS = [
+  {
+    title: "Platform",
+    description: "Hire, manage, and pay your global teams with Remote",
+    items: [
+      {
+        title: "Employer of Record (EOR)",
+        description:
+          "Simply hire, manage and pay employees in any country where you don't have a legal entity.",
+        url: null,
+      },
+      {
+        title: "Contractor Management",
+        description:
+          "Manage your contractors, automate admin and ensure swift payments in more than 200 countries and territories.",
+        url: null,
+      },
+      {
+        title: "Payroll",
+        description:
+          "Consolidate your multi-country payroll process and pay your entire team quickly, accurately and compliantly.",
+        url: null,
+      },
+      {
+        title: "HR Management",
+        description:
+          "Manage your entire global workforce in one place, freeing up your time and money so you can grow your business.",
+        url: null,
+      },
+      {
+        title: "Remote Talent",
+        description:
+          "Struggling to find the right candidates for hard-to-fill roles? Remote Talent is here to help.",
+        url: null,
+      },
+      {
+        title: "Remote Embedded & APIs",
+        description: "",
+        url: "https://remote.com/global-hr/remote-api",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    description: "The knowledge you need to go global with confidence.",
+    items: [
+      {
+        title: "Tech & Tools",
+        items: [
+          {
+            title: "Compare Remote",
+            url: "https://remote.com/resources/compare-remote",
+          },
+          {
+            title: "Employee cost calculator",
+            url: "https://remote.com/resources/employee-cost-calculator",
+          },
+          {
+            title: "Misclassification risk tool",
+            url: "https://remote.com/resources/employee-misclassification-risk-tool",
+          },
+          {
+            title: "Contractor Payout Explorer",
+            url: "https://remote.com/resources/contractor-payout-explorer",
+          },
+          {
+            title: "HR Integrations",
+            url: "https://remote.com/resources/integrations-for-customers",
+          },
+          {
+            title: "Developer API docs",
+            url: "https://remote.com/resources/api/getting-started",
+          },
+        ],
+      },
+      {
+        title: "Global HR Library",
+        items: [
+          {
+            title: "Blog",
+            url: "https://remote.com/blog",
+          },
+          {
+            title: "Customer Stories",
+            url: "https://remote.com/resources/case-studies",
+          },
+          {
+            title: "Research",
+            url: "https://remote.com/resources/research",
+          },
+          {
+            title: "Expert Sessions",
+            url: "https://remote.com/resources/webinars",
+          },
+          {
+            title: "HR Glossary",
+            url: "https://remote.com/resources/hr-glossary",
+          },
+          {
+            title: "Job Description Template",
+            url: "https://remote.com/resources/job-descriptions",
+          },
+        ],
+      },
+      {
+        title: "Remote For You",
+        items: [
+          {
+            title: "US Expansion Package",
+            url: "https://remote.com/global-hr/us-expansion",
+          },
+          {
+            title: "Switch to Remote",
+            url: "https://remote.com/global-hr/switching-to-remote-hr",
+          },
+          {
+            title: "Startups",
+            url: "https://remote.com/global-hr/startups",
+          },
+          {
+            title: "Small business",
+            url: "https://remote.com/global-hr/small-business",
+          },
+          {
+            title: "Midsize business",
+            url: "https://remote.com/global-hr/midsize",
+          },
+          {
+            title: "Enterprise",
+            url: "https://remote.com/global-hr/enterprise",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Pricing",
+    description:
+      "Remote's Fair Price Guarantee means no hidden fees, deposits, or surprises",
+    url: "https://remote.com/pricing",
+  },
+  {
+    title: "Global Insights",
+    description:
+      "Global employment guides & tools for everywhere you want to hire.",
+    illustration: "/illustration/Country_Explorer.png",
+    items: [
+      {
+        title: "Employment Guides",
+        items: [
+          {
+            title: "Country Explorer",
+            url: "https://remote.com/country-explorer",
+          },
+          {
+            title: "US State Explorer",
+            url: "https://remote.com/us-state-explorer",
+          },
+        ],
+      },
+      {
+        title: "Scaling Strategies",
+        items: [
+          {
+            title: "US Expansion Package",
+            url: "https://remote.com/global-hr/us-expansion",
+          },
+          {
+            title: "Global Hiring Planner",
+            url: "https://remote.com/resources/global-employment-country-selector",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Why Remote",
+    description: "See why companies around the world trust Remote.",
+    items: [
+      {
+        title: "Advantages of Remote",
+        items: [
+          {
+            title: "Global infrastructure",
+            url: "https://remote.com/why-remote/global-infrastructure",
+          },
+          {
+            title: "Fair price guarantee",
+            url: "https://remote.com/blog/fair-price-guarantee",
+          },
+          {
+            title: "Security & Compliance",
+            url: "https://remote.com/why-remote/security-compliance",
+          },
+          {
+            title: "IP Protection",
+            url: "https://remote.com/why-remote/ip-protection",
+          },
+          {
+            title: "Customer Experience",
+            url: "https://remote.com/why-remote/customer-experience",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "About",
+    description: "Learn about Remote and find opportunities to connect.",
+    illustration: "/illustration/Purple_Globe_Optimised.svg",
+    items: [
+      {
+        title: "Connect with Remote",
+        items: [
+          {
+            title: "Events",
+            url: "https://remote.com/events",
+          },
+          {
+            title: "Careers",
+            url: "https://remote.com/careers",
+          },
+          {
+            title: "Support",
+            url: "https://support.remote.com/hc/en-us",
+          },
+          {
+            title: "Contact us",
+            url: "https://remote.com/contact-us",
+          },
+          {
+            title: "Newsroom",
+            url: "https://remote.com/news",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Partners",
+    description: "Let's build the future of work together.",
+    items: [
+      {
+        title: "Partner with Remote",
+        items: [
+          {
+            title: "Become a Partner",
+            url: "https://remote.com/partners/become-a-partner",
+          },
+          {
+            title: "Partner Programs",
+            url: "https://remote.com/partners",
+          },
+          {
+            title: "Browser Our Partner Marketplace",
+            url: "https://remote.com/partners/marketplace",
+          },
+          {
+            title: "Browser Our Recruiter Network",
+            url: "https://remote.com/recruiters",
+          },
+          {
+            title: "Remote and Gusto",
+            url: "https://remote.com/integrations/remote-gusto-small-business-global-expansion",
+          },
+        ],
+      },
+    ],
+  },
+];
