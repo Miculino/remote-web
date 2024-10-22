e330fba18d1cb1f40e85020cb7804f166e4f0e02
RemusDB <remusd.buhaianu@gmail.com>
2024-10-22 09:37:59 +0300

    chore: update favicon

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${crimsonPro.variable} ${poppins.variable} antialiased`}
       >
